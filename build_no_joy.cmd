b9affc08be63b01b67575e7165ac38ab9a153445
Gian Luca De Michelis <gianluca.demichelis@eng.it>
2025-02-11 15:41:04 +0100

    modifica script di build

--- build_no_joy.cmd
+++ build_no_joy.cmd
@@ -1,5 +1,7 @@
 del pacman.tap
 del pacman.wav
+del pacman_k.tap
+del pacman_k.wav
 zcc +ace -lndos -O2 --opt-code-speed=inlineints -pragma-define:CLIB_EXIT_STACK_SIZE=0 -UUSEJOYSTICK -create-app -Cz--audio --generic-console pacman.c -opacman
 move pacman.tap pacman_k.tap
 move pacman.wav pacman_k.wav
--- build_w_joy.cmd
+++ build_w_joy.cmd
@@ -1,5 +1,7 @@
 del pacman.tap
 del pacman.wav
+del pacman_j.tap
+del pacman_j.wav
 zcc +ace -lndos -O2 --opt-code-speed=inlineints -pragma-define:CLIB_EXIT_STACK_SIZE=0 -DUSEJOYSTICK=1 -create-app -Cz--audio --generic-console pacman.c -opacman
 move pacman.tap pacman_j.tap
 move pacman.wav pacman_j.wav
